569746e8d0442fff13a5ff098f26ba37ac215fed
trantuhao <tuhao99@gmail.com>
2020-12-09 23:57:37 +0700

    add / route

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set("view engine", "hbs");
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -30,31 +30,31 @@ app.use("/user", userApi);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 // connect db
 mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connected successfully");
-  })
-  .catch((error) => {
-    console.log("Error connecting to database");
-  });
+    .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch((error) => {
+        console.log("Error connecting to database");
+    });
 
 module.exports = app;
--- components/auth/authApi.js
+++ components/auth/authApi.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authController = require('./authController');
 
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/login-google', authController.loginGoogle);
