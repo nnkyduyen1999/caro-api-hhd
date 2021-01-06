ba795d4415930cfd9deb89d7d4747843c0fad2c6
trantuhao <tuhao99@gmail.com>
2021-01-06 16:50:20 +0700

    complete xin thua function

--- socket.io/socket.io.js
+++ socket.io/socket.io.js
@@ -3,79 +3,81 @@ const roomDAL = require("../components/room/roomDAL");
 let onlineUsers = [];
 let matchingUsers = [];
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-
+const GIVEN_IN_EVENT = "give-in"
 module.exports = (io, socket) => {
-  //join a chat room
-  const { roomId } = socket.handshake.query;
-  socket.join(roomId);
+    //listen for new connection
+    socket.on("new-connection", (user) => {
+        if (!onlineUsers.some((item) => item.userId === user._id)) {
+            onlineUsers.push({
+                userId: user._id,
+                socketId: socket.id,
+                username: user.username,
+            });
+            io.emit("update-online-users", onlineUsers);
+        }
+    });
 
-  // Listen for new messages
-  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
-    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
-  });
+    socket.on("matching", async (user) => {
+        if (matchingUsers.length >= 1) {
+            //create room
+            const createdRoom = await roomDAL.insert(
+                matchingUsers[0].userId,
+                user._id
+            );
 
-  //listen for new connection
-  socket.on("new-connection", (user) => {
-    if (!onlineUsers.some((item) => item.userId === user._id)) {
-      onlineUsers.push({
-        userId: user._id,
-        socketId: socket.id,
-        username: user.username,
-      });
-      io.emit("update-online-users", onlineUsers);
-    }
-  });
+            data = {
+                _id: createdRoom._id,
+                userXId: createdRoom.userXId,
+                usernameX: matchingUsers[0].username,
+                userOId: createdRoom.userOId,
+                usernameO: user.username,
+            };
 
-  socket.on("matching", async (user) => {
-    if (matchingUsers.length >= 1) {
-      //create room
-      const createdRoom = await roomDAL.insert(
-        matchingUsers[0].userId,
-        user._id
-      );
+            //send message to users
+            socket.broadcast
+                .to(matchingUsers[0].socketId)
+                .emit("successfullyMatched", data);
+            socket.emit("successfullyMatched", data);
 
-      data = {
-        _id: createdRoom._id,
-        userXId: createdRoom.userXId,
-        usernameX: matchingUsers[0].username,
-        userOId: createdRoom.userOId,
-        usernameO: user.username,
-      };
+            matchingUsers.shift();
+        } else {
+            if (!matchingUsers.some((item) => item._id === user._id)) {
+                matchingUsers.push({
+                    userId: user._id,
+                    socketId: socket.id,
+                    username: user.username,
+                });
+            }
+        }
+    });
 
-      //send message to users
-      socket.broadcast
-        .to(matchingUsers[0].socketId)
-        .emit("successfullyMatched", data);
-      socket.emit("successfullyMatched", data);
+    //Join a room
+    socket.on("joinRoom", (roomId) => {
+        socket.join(roomId);
+    });
 
-      matchingUsers.shift();
-    } else {
-      if (!matchingUsers.some((item) => item._id === user._id)) {
-        matchingUsers.push({
-          userId: user._id,
-          socketId: socket.id,
-          username: user.username,
-        });
-      }
-    }
-  });
+    socket.on("requestMove", (data) => {
+        // console.log("req", data, data.roomId);
+        io.to(data.roomId).emit("acceptedMove", data);
+    });
 
-  socket.on("joinRoom", (roomId) => {
-    socket.join(roomId);
-  });
+    // Listen for new messages
+    socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+        io.in(data.roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+    });
 
-  socket.on("requestMove", (data) => {
-    console.log("req", data, data.roomId);
-    io.to(data.roomId).emit("acceptedMove", data);
-  });
+    socket.on(GIVEN_IN_EVENT, (data) => {
+        console.log("req", data);
+        io.in(data.roomId).emit(GIVEN_IN_EVENT, data);
+    });
 
-  socket.on("disconnect", (user) => {
-    if (onlineUsers.some((item) => item._id === user._id)) {
-      const temp = onlineUsers.filter((item) => item._id !== user._id);
-      onlineUsers = [...temp];
-      io.emit("update-online-users", onlineUsers);
-    }
-    // Leave the room if the user closes the socket
-    socket.leave(roomId);
-  });
+    socket.on("disconnect", (user) => {
+        if (onlineUsers.some((item) => item._id === user._id)) {
+            const temp = onlineUsers.filter((item) => item._id !== user._id);
+            onlineUsers = [...temp];
+            io.emit("update-online-users", onlineUsers);
+        }
+        // Leave the room if the user closes the socket
+        socket.leave(roomId);
+    });
 };
