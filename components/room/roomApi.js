e01dec08f921431344ca09ff12e8a25ef4bbc58f
trantuhao <tuhao99@gmail.com>
2021-01-09 16:10:17 +0700

    api load room by id

--- components/room/roomApi.js
+++ components/room/roomApi.js
@@ -4,6 +4,8 @@ const roomController = require('./roomController')
 
 router.get('/', roomController.all)
 
+router.get('/:id', roomController.getById)
+
 // router.post('/', roomDAL.testRoom);
 
 module.exports = router
--- components/room/roomController.js
+++ components/room/roomController.js
@@ -3,27 +3,40 @@ const UserDAL = require("../user/userDAL");
 const Room = require("./roomModel");
 
 module.exports = {
-  all: async (req, res, next) => {
-    try {
-      const rooms = await RoomDAL.all();
-      const roomsWithUsername = [];
-      for (let item of rooms) {
-        const xPlayer = item.xCurrentPlayer
-          ? await UserDAL.loadUsernameById(item.xCurrentPlayer)
-          : "";
-        const oPlayer = item.oCurrentPlayer
-          ? await UserDAL.loadUsernameById(item.oCurrentPlayer)
-          : "";
-        roomsWithUsername.push({
-          ...item._doc,
-          xPlayerUsername: xPlayer ? xPlayer.username : "",
-          oPlayerUsername: oPlayer ? oPlayer.username : "",
-        });
-      }
+    all: async (req, res, next) => {
+        try {
+            const rooms = await RoomDAL.all();
+            const roomsWithUsername = [];
+            for (let item of rooms) {
+                const xPlayer = item.xCurrentPlayer
+                    ? await UserDAL.loadUsernameById(item.xCurrentPlayer)
+                    : "";
+                const oPlayer = item.oCurrentPlayer
+                    ? await UserDAL.loadUsernameById(item.oCurrentPlayer)
+                    : "";
+                roomsWithUsername.push({
+                    ...item._doc,
+                    xPlayerUsername: xPlayer ? xPlayer.username : "",
+                    oPlayerUsername: oPlayer ? oPlayer.username : "",
+                });
+            }
 
-      res.send(roomsWithUsername);
-    } catch (err) {
-      res.send(err);
+            res.send(roomsWithUsername);
+        } catch (err) {
+            res.send(err);
+        }
+    },
+    getById: async (req, res, next) => {
+        const roomId = req.params.id;
+
+        try {
+            const room = await Room.findById(roomId);
+            if(room) {
+                return res.status(200).json({room});
+            }
+            return res.status(400).json({message: 'room not found'})
+        } catch (err) {
+            res.status(500).send(err);
+        }
     }
-  },
 };
